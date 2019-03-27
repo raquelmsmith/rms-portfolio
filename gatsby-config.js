9a45c77098c47f1877501aee915ca386984636ef
Raquel Smith <raquel@foodbloggerpro.com>
2019-03-27 16:25:24 -0700

    Discourage robots and indexing

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,5 +47,11 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        policy: [{ userAgent: '*', disallow: '/' }]
+      }
+    },
   ],
 }
