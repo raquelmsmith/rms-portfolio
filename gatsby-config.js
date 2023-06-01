434f56f8a0691ab9633c807dd0287190ed9b5f94
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-05-31 20:12:04 -0700

    metadata and readme updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby TailwindCSS Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter with TailwindCSS and common dev tools. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@kosvrouvas`,
+    title: `Raquel M Smith - Software engineer & product manager`,
+    description: `Raquel M Smith is a software engineer and product manager based in San Luis Obispo, CA.`,
+    author: `@raquelmsmith`,
   },
   flags: {
   },
