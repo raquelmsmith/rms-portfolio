2e21660c3ab1b84c4bf6518126e448acb2e2d8ff
Raquel Smith <raquelmsmith@users.noreply.github.com>
2023-06-04 10:26:27 -0700

    correct silly slogan

--- src/pages/words.tsx
+++ src/pages/words.tsx
@@ -28,7 +28,7 @@ const WordsPage = ({
                     </h1>
                     <main className="mt-12 max-w-2xl text-center">
                         <p>
-                            I don't write that often, but when I do{" "}
+                            I don't always write down my thoughts, but when I do{" "}
                             <span className="line-through opacity-50">
                                 I drink Dos Equis
                             </span>{" "}
